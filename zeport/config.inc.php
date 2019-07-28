90021664b8925689d192b639033986bfb8586d27
Luugui <luisguilhermester@live.com>
2019-07-28 20:02:50 -0300

    Update config.inc.php

--- zeport/config.inc.php
+++ zeport/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 //CONFIGURABLE
 # zabbix server info(user must have API access)
-$z_server   = 'http://172.31.0.236/zabbix/api_jsonrpc.php';
-$z_user		  = 'user-premierit';
-$z_pass     = 'SaCvWojFV43@19';
+$z_server   = 'http://zabbixserver/zabbix/api_jsonrpc.php';
+$z_user		  = 'Admin';
+$z_pass     = 'zabbix';
 $z_path     = '/usr/share/zabbix';
 
 ?>
